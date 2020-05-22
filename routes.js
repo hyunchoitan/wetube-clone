d16e9a7d557a29d33847082d36b3c864ab8905f3
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-05-22 21:28:37 +0800

    4th commit : creating templates files, change respond from send to render using pug

--- routes.js
+++ routes.js
@@ -9,16 +9,16 @@ const HOME = "/",
 // Users
 
 const USERS = "/users",
-  USER_DETAIL = "/:id",
   EDIT_PROFILE = "/edit-profile",
-  CHANGE_PASSWORD = "/change-password";
+  CHANGE_PASSWORD = "/change-password",
+  USER_DETAIL = "/:id";
 
 // Videos
 
 const VIDEOS = "/videos",
   UPLOAD = "/upload",
   VIDEO_DETAIL = "/:id",
-  EDIT_VIDEO = ":id/edit",
+  EDIT_VIDEO = "/:id/edit",
   DELETE_VIDEO = "/:id/delete";
 
 const routes = {
@@ -28,9 +28,9 @@ const routes = {
   logout: LOGOUT,
   search: SEARCH,
   users: USERS,
-  userDetail: USER_DETAIL,
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
+  userDetail: USER_DETAIL,
   videos: VIDEOS,
   upload: UPLOAD,
   videoDetail: VIDEO_DETAIL,
