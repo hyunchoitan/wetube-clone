badca864b2c1dbf8c61e5a6495730f134653d12b
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-05-22 14:31:31 +0800

    3rd commit
    : creating server, routers, routes, controllers.

---
+++ routes.js
@@ -0,0 +1,41 @@
+// Global
+
+const HOME = "/",
+  JOIN = "/join",
+  LOGIN = "/login",
+  LOGOUT = "/logout",
+  SEARCH = "/search";
+
+// Users
+
+const USERS = "/users",
+  USER_DETAIL = "/:id",
+  EDIT_PROFILE = "/edit-profile",
+  CHANGE_PASSWORD = "/change-password";
+
+// Videos
+
+const VIDEOS = "/videos",
+  UPLOAD = "/upload",
+  VIDEO_DETAIL = "/:id",
+  EDIT_VIDEO = ":id/edit",
+  DELETE_VIDEO = "/:id/delete";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO,
+};
+
+export default routes;
