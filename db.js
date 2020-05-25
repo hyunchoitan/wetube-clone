ba7a78301e6343bc995076489feae47976a88b52
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-05-25 17:38:44 +0800

    11th commit:
    Home controller, Join controller(add POST method, error verify)

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video Awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 1212121,
+    title: "Video Super",
+    description: "This is something I love",
+    views: 14,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 555555,
+    title: "Video Nice",
+    description: "This is something I love",
+    views: 34,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 111111,
+    title: "Video Perfect",
+    description: "This is something I love",
+    views: 64,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+];
