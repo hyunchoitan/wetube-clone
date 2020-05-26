da26086345ca46d8953d156c5cbabd9b7ff59adf
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-05-26 18:12:42 +0800

    13th commit : Install and set MongoDB using Mongoose, Hide key using dot env.

--- db.js
+++ db.js
@@ -1,54 +1,17 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Video Awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 1212121,
-    title: "Video Super",
-    description: "This is something I love",
-    views: 14,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 555555,
-    title: "Video Nice",
-    description: "This is something I love",
-    views: 34,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-  {
-    id: 111111,
-    title: "Video Perfect",
-    description: "This is something I love",
-    views: 64,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("☑️  Connected to DB!");
+const handleError = (error) =>
+  console.log(`✖️  Error on DB Connection: ${error}`);
+db.once("open", handleOpen);
+db.on("error", handleError);
