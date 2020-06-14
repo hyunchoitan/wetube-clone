dedb7d07ba335e2db213ff11a553f5bd5fd71f95
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2020-06-14 22:06:03 +0800

    29th commit : Add Volumnup/Mute, Fullscreen JSfuntionality to videoplayer.

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,46 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumnBtn = document.getElementById("jsVolumnBtn");
+const fullScreenBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
+}
+
+function handleVolumnClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumnBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumnBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
+function exitFullScreen() {
+  fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenBtn.addEventListener("click", goFullScreen);
+  document.exitFullscreen();
+}
+
+function goFullScreen() {
+  videoContainer.requestFullscreen();
+  fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenBtn.removeEventListener("click", goFullScreen);
+  fullScreenBtn.addEventListener("click", exitFullScreen);
 }
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumnBtn.addEventListener("click", handleVolumnClick);
+  fullScreenBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
